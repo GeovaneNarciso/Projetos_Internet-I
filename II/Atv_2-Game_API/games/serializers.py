0370441c3ec8b610fc5f9385d6c0700a3738c8ef
GeovaneNarciso <geovane.nivel4@gmail.com>
2019-10-02 10:27:01 -0300

    Atualização

--- II/Atv_2-Game_API/games/serializers.py
+++ II/Atv_2-Game_API/games/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 from games.models import Game
+from datetime import datetime
 
 
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'name', 'release_date', 'game_category')
+
+    def validate_name(self, data):
+        game = Game.objects.filter(name=data)
+        if game:
+            msg = "Jogos não podem ter nomes iguais."
+            raise serializers.ValidationError(msg)
+        return data
+
+    def validate_delete(self, game):
+        if game.release_date >= datetime.now():
+            return True
+        else:
+            msg = "Jogos já lançados não podem ser deletados."
+            raise serializers.ValidationError(msg)
--- II/Atv_2-Game_API/games/views.py
+++ II/Atv_2-Game_API/games/views.py
@@ -1,9 +1,3 @@
-"""
-Book: Building RESTful Python Web Services
-Chapter 2: Working with class based views and hyperlinked APIs in Django
-Author: Gaston C. Hillar - Twitter.com/gastonhillar
-Publisher: Packt Publishing Ltd. - http://www.packtpub.com
-"""
 #  from rest_framework.parsers import JSONParser
 from rest_framework import status
 from rest_framework.decorators import api_view
